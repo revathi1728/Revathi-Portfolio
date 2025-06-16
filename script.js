0c0eae9d6681adc51b4e0dc79f90f0a264d3f095
revathi1728 <miriyalarevathi1728@gmail.com>
2025-06-16 20:55:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional smooth scroll animation on click
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target?.scrollIntoView({ behavior: 'smooth' });
+  });
+});
